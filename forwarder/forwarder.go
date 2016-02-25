28ec8db10fc484bca1d8dc968b88f7164dc53fb3
Ondřej Kupka <ondra.cap@gmail.com>
2016-02-25 09:14:53 +0100

    wip [ci skip]

--- forwarder/forwarder.go
+++ forwarder/forwarder.go
@@ -12,12 +12,21 @@ import (
 	"gopkg.in/tomb.v2"
 )
 
+const (
+	// Every message must be written to the peer in less than 10 seconds.
+	writeTimeout = 10 * time.Second
+
+	// Send PING every minute. Compute the read timeout based on that.
+	pingPeriod  = 1 * time.Minute
+	pongTimeout = pingPeriod / 10 * 11
+)
+
 type Forwarder struct {
 	ws *websocket.Conn
 	t  tomb.Tomb
 }
 
-func Forward(changesAddr string, forwardCh chan<- *common.Sensor) (*Forwarder, error) {
+func Forward(changesAddr string, forwardCh chan<- *common.SensorState) (*Forwarder, error) {
 	// Connect to the remote address.
 	dialer := &websocket.Dialer{
 		HandshakeTimeout: 5 * time.Second,
