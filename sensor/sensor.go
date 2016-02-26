fde80b5549a7953ac369d9fbbd71921e79e29f90
Ondřej Kupka <ondra.cap@gmail.com>
2016-02-26 16:19:02 +0100

    wip [ci skip]

--- sensor/sensor.go
+++ sensor/sensor.go
@@ -1,46 +1,8 @@
 package sensors
 
-import (
-	// Stdlib
-	"encoding/json"
-	"os"
-
-	// Internal
-	"github.com/FlushCapacitor/flush-capacitor/sensors/rpi"
-	"github.com/FlushCapacitor/flush-capacitor/sensors/spec"
-)
-
 type Sensor interface {
 	Name() string
 	State() string
 	Watch(func()) error
 	Close() error
-}
-
-func FromSpec(ds *spec.DeviceSpec) (Sensor, error) {
-	// Check the spec.
-	if err := ds.Validate(); err != nil {
-		return nil, err
-	}
-
-	// Create a sensor based on the spec.
-	return rpi.SensorFromSpec(ds)
-}
-
-func FromSpecFile(filename string) (Sensor, error) {
-	// Open the spec file.
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	// Read and parse the spec file.
-	var ds spec.DeviceSpec
-	if err := json.NewDecoder(file).Decode(&ds); err != nil {
-		return nil, err
-	}
-
-	// Pass the spec to FromSpec.
-	return FromSpec(&ds)
 }
